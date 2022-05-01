cd0fa999be4f23b3854942abb997dd0eb5fe60d7
GusFors <gf222cy@student.lnu.se>
2022-05-01 03:04:58 +0200

    use timeouts while still writing all files between some function calls

--- app.js
+++ app.js
@@ -9,7 +9,23 @@ const gm = require('gm')
   //     })
   //   })
   upScale('hitcircle@2x.png')
+  upScale('hitcircleoverlay@2x.png')
+  gm('default-1@2x.png').write('default-1@2x.pngresize.png', (err) => {})
 
+  setTimeout(() => {
+    gm('hitcircle@2x.pngresize.png')
+      .composite('default-1@2x.pngresize.png')
+      .gravity('Center')
+      //.composite('hitcircleoverlay@2x.pngresize.png')
+      .write('defaultTest.png' + 'resize.png', (err) => {})
+    // .append('hitcircleoverlay@2x.pngresize.png', true)
+
+    setTimeout(() => {
+      gm('defaultTest.pngresize.png')
+        .composite('hitcircleoverlay@2x.pngresize.png')
+        .write('defaultTest2.png' + 'resize.png', (err) => {})
+    }, 500)
+  }, 500)
   // upscale  hitcircle and hitcircleoverlay by 1.25
   //   console.log(hitCircleInfo.width)
   //   gm('hitcircle@2x.png').resize(await hitCircleInfo.width, await hitCircleInfo.height)
